f0600fbe3ea308c140c6ddb5dd5b3efa35a8d55a
developer <youngmeen101@sgrsoft.com>
2021-07-06 18:16:16 +0900

    first commit

---
+++ CardTableView/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  CardTableView
+//
+//  Created by developer on 2021/07/06.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var clockLabel: UILabel!
+    @IBOutlet weak var homeImg: UIImageView!
+    
+}
+
---
+++ CardTableView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CardTableView
+//
+//  Created by developer on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
+
